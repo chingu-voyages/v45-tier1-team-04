52b136a11410f5cbd3a1f847e7b75b21ad831c30
Milica Maljokovic <milicamaljokovic@gmail.com>
2023-08-12 13:21:57 +0200

    added fonts, padding, font-size, landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Header from './components/Header'
+import Landing from './components/Landing'
 function App() {
   
 
   return (
-    <>
+    <div className='app w-[100vw] h-[100vh] overflow-x-hidden overflow-y-scroll relative'>
       
       <Header />
+      <Landing />
 
-    </>
+    </div>
   )
 }
 
