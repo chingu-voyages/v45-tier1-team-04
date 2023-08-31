97e5bba1e8f727e2ae939713005560ef3bef41ee
biroue <biroueisaac@gmail.com>
2023-08-31 13:48:34 +0100

    Adding button and hr to our card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,7 +13,13 @@ function Card() {
   return (
     <>
       <div className="App">
-        <h1 className="text-center	">using JavaScript inbuilt FETCH API</h1>
+        <h1 className="text-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          Fireball{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            {" "}
+            FETCH API
+          </span>
+        </h1>
         <center className="grid grid-cols-3 gap-y-12	">
           {data.map((dataObj) => {
             return (
@@ -46,6 +52,10 @@ function Card() {
                   <div className="font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.reclong}
                   </div>
+                  <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    More
+                  </button>
                 </p>
               </div>
             );
