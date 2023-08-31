83629a9c8e127b90c78aa1b5aa41871711def376
Drazen Drinic <drazendrinic@hotmail.com>
2023-08-31 05:00:00 -0500

    add: contributors data file

---
+++ src/assets/contributors.js
@@ -0,0 +1,24 @@
+const contributors = [
+  {
+    gitHubName: "Drazen-TheGRC",
+    gitHubLink: "https://github.com/Drazen-TheGRC",
+  },
+  {
+    gitHubName: "biroue10",
+    gitHubLink: "https://github.com/biroue10",
+  },
+  {
+    gitHubName: "maggickkw",
+    gitHubLink: "https://github.com/maggickkw",
+  },
+  {
+    gitHubName: "FWDcodeStorage",
+    gitHubLink: "https://github.com/FWDcodeStorage",
+  },
+  {
+    gitHubName: "Uchenna Ezeonuogu",
+    gitHubLink: "",
+  },
+];
+
+export default contributors;
