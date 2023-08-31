ee51a870d0f1520588fdcdbd146b4723c905a6f0
biroue <biroueisaac@gmail.com>
2023-08-31 13:39:43 +0100

    refactor the card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import "./Header";
-import "./Footer";
 const url = "https://data.nasa.gov/resource/gh4g-9sfh.json";
 function Card() {
   const [data, setData] = useState([]);
@@ -9,34 +7,52 @@ function Card() {
       .then((res) => res.json())
       .then((d) => setData(d));
   };
-  console.log(fetchInfo);
   useEffect(() => {
     fetchInfo();
   }, []);
   return (
-    <div className="App">
-      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
-      <center>
-        {data.map((dataObj) => {
-          return (
-            <div key={dataObj.id} className="grid-cols-3">
-              <p
-                href="#"
-                className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-              >
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {dataObj.name}
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  {dataObj.nametype}
-                  {dataObj.id}
+    <>
+      <div className="App">
+        <h1 className="text-center	">using JavaScript inbuilt FETCH API</h1>
+        <center className="grid grid-cols-3 gap-y-12	">
+          {data.map((dataObj) => {
+            return (
+              <div key={dataObj.id}>
+                <p
+                  href="#"
+                  className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                >
+                  <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {dataObj.name}
+                  </div>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {dataObj.nametype}
+                  </div>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {dataObj.recclass}
+                  </div>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {dataObj.mass}
+                  </div>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {dataObj.fall}
+                  </div>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {dataObj.year}
+                  </div>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {dataObj.reclat}
+                  </div>
+                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                    {dataObj.reclong}
+                  </div>
                 </p>
-              </p>
-            </div>
-          );
-        })}
-      </center>
-    </div>
+              </div>
+            );
+          })}
+        </center>
+      </div>
+    </>
   );
 }
 export default Card;
