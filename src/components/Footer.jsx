6b4152aef0d35db56124ded94663466359569a02
Drazen Drinic <drazendrinic@hotmail.com>
2023-08-31 05:40:55 -0500

    Add/Extract: ContributorList component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import "../App.css";
+import contributors from "../assets/contributors";
 //you imported just App.css
 //you shouldn't create sepatate folder for Footer (footer.css), you should write all in App.css just use unique id or className for footer component
 //and footer shoud be positioned absolute or fixed like navbar
@@ -9,52 +10,42 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 //then you can import the same way {name of icon} from 'react-icons/fa -if it's fontawesome' and than use in project
 //like <nameOfIcon />
 export default function Footer() {
+  const sortedContributors = contributors.sort((a, b) =>
+    a.gitHubName.localeCompare(b.gitHubName)
+  );
+
+  console.log("SORTED" + sortedContributors);
+
   return (
-    <div id="footer" className="flex justify-between xl:text-[1rem] md:text-[.8rem] sm:text-[.5rem] absolute left-0 right-0 bottom-0 w-full overflow-hidden md:px-[.5em] md:py-[.2em] bg-[#00000060] text-white font-thin">
+    <div
+      id="footer"
+      className="flex justify-between xl:text-[1rem] md:text-[.8rem] sm:text-[.5rem] absolute left-0 right-0 bottom-0 w-full overflow-hidden md:px-[.5em] md:py-[.2em] bg-[#00000060] text-white font-thin"
+    >
       <div>
         <p>Search</p>
         <p>NASA Blogs</p>
         <p>NASA Gallery</p>
         <p>Meteorite Landings</p>
       </div>
-      <div className="flex justify-center items-end">&#169; 2023 V45-Team04 | All Rights Reserved</div>
-      <div className="w-[6em]">
-        {/* this should be in a div so we can position it better */}
-        <div className="flex justify-between">
-        <p className="w-[80%]">
-          biroue
-        </p>
-        <span><FontAwesomeIcon icon={faGithub} /></span>
-        </div>
-      
-        {/* this should be in a div so we can position it better */}
-        <div className="flex justify-between">
-        <p className="w-[80%]">
-          Drazen
-        </p>
-        <span><FontAwesomeIcon icon={faGithub} /></span>
-        </div>
-
-        <div className="flex justify-between">
-        <p className="w-[80%]">
-          Uche
-        </p>
-        <span><FontAwesomeIcon icon={faGithub} /></span>
-        </div>
-        <div className="flex justify-between">
-        <p className="w-[80%]">
-         Milica
-        </p>
-        <span><FontAwesomeIcon icon={faGithub} /></span>
-        </div>
-        <div className="flex justify-between">
-        <p className="w-[80%]">
-         David
-        </p>
-        <span><FontAwesomeIcon icon={faGithub} /></span>
-        </div>
-        
+      <div className="flex justify-center items-end">
+        &#169; 2023 V45-Team04 | All Rights Reserved
       </div>
+      <div className="w-[13em]">
+        {sortedContributors.map((contributor) => (
+          <ContributorList contributor={contributor} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ContributorList({ contributor }) {
+  return (
+    <div className="flex justify-between">
+      <p className="w-[80%]">{contributor.gitHubName}</p>
+      <span>
+        <FontAwesomeIcon icon={faGithub} />
+      </span>
     </div>
   );
 }
