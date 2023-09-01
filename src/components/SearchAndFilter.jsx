85cad81df5d72ea4eb8947c2b6f803a74b8c4169
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-01 04:51:33 -0500

    Refactor: SearchAndFilter component to use one state object and one handleFormChange function

--- src/components/SearchAndFilter.jsx
+++ src/components/SearchAndFilter.jsx
@@ -1,74 +1,102 @@
 import { useState } from "react";
 
 function SearchAndFilter() {
-  const [search, setSearch] = useState("");
-  const [startYear, setStartYear] = useState(1900);
-  const [endYear, setEndYear] = useState(new Date().getFullYear());
-  const [minMass, setMinMass] = useState();
-  const [maxMass, setMaxMass] = useState();
+  const [formData, setformData] = useState({
+    search: "",
+    startYear: 1900,
+    endYear: new Date().getFullYear(),
+    minMass: null,
+    maxMass: null,
+  });
 
-  function handleSearchChange(event) {
-    setSearch(event.target.value);
+  console.log(formData);
+
+  function handleFormChange(event) {
+    setformData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
+
+  function handleFormFix(event, fixValue) {
+    setformData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: fixValue,
+      };
+    });
+  }
+
+  //   const [search, setSearch] = useState("");
+  //   const [startYear, setStartYear] = useState(1900);
+  //   const [endYear, setEndYear] = useState(new Date().getFullYear());
+  //   const [minMass, setMinMass] = useState();
+  //   const [maxMass, setMaxMass] = useState();
+
+  //   function handleSearchChange(event) {
+  //     setSearch(event.target.value);
+  //   }
   // Handle Start Year
-  function handleStartYearChange(event) {
-    setStartYear(parseInt(event.target.value));
-  }
+  //   function handleStartYearChange(event) {
+  //     setStartYear(parseInt(event.target.value));
+  //   }
   // Handle Start Year wrong value
   function fixStartYear(event) {
     let tempStartYear;
     if (parseInt(event.target.value) < 1900) {
       tempStartYear = 1900;
-    } else if (parseInt(event.target.value) > endYear) {
-      tempStartYear = endYear;
+    } else if (parseInt(event.target.value) > formData.endYear) {
+      tempStartYear = formData.endYear;
     } else {
       tempStartYear = parseInt(event.target.value);
     }
 
-    setStartYear(tempStartYear);
+    handleFormFix(event, tempStartYear);
   }
   // Handle End Year
-  function handleEndYearChange(event) {
-    setEndYear(parseInt(event.target.value));
-  }
+  //   function handleEndYearChange(event) {
+  //     setEndYear(parseInt(event.target.value));
+  //   }
   // Handle End Year wrong value
   function fixEndYear(event) {
     let tempEndYear;
     if (parseInt(event.target.value) > new Date().getFullYear()) {
       tempEndYear = new Date().getFullYear();
-    } else if (parseInt(event.target.value) < startYear) {
-      tempEndYear = startYear;
+    } else if (parseInt(event.target.value) < formData.startYear) {
+      tempEndYear = formData.startYear;
     } else {
       tempEndYear = parseInt(event.target.value);
     }
-    setEndYear(tempEndYear);
+    handleFormFix(event, tempEndYear);
   }
 
-  function handleMinMassChange(event) {
-    setMinMass(parseInt(event.target.value));
-  }
+  //   function handleMinMassChange(event) {
+  //     setMinMass(parseInt(event.target.value));
+  //   }
   function fixMinMass(event) {
     let tempMinMass;
     if (parseInt(event.target.value) < 0) {
       tempMinMass = 0;
-    } else if (parseInt(event.target.value) > maxMass) {
-      tempMinMass = maxMass;
+    } else if (parseInt(event.target.value) > formData.maxMass) {
+      tempMinMass = formData.maxMass;
     } else {
       tempMinMass = parseInt(event.target.value);
     }
-    setMinMass(tempMinMass);
+    handleFormFix(event, tempMinMass);
   }
-  function handleMaxMassChange(event) {
-    setMaxMass(parseInt(event.target.value));
-  }
+  //   function handleMaxMassChange(event) {
+  //     setMaxMass(parseInt(event.target.value));
+  //   }
   function fixMaxMass(event) {
     let tempMaxMass;
-    if (parseInt(event.target.value) < minMass) {
-      tempMaxMass = minMass;
+    if (parseInt(event.target.value) < formData.minMass) {
+      tempMaxMass = formData.minMass;
     } else {
       tempMaxMass = parseInt(event.target.value);
     }
-    setMaxMass(tempMaxMass);
+    handleFormFix(event, tempMaxMass);
   }
 
   return (
@@ -78,8 +106,8 @@ function SearchAndFilter() {
         name="search"
         type="text"
         placeholder="Your Search"
-        onChange={handleSearchChange}
-        value={search}
+        onChange={handleFormChange}
+        value={formData.search}
       />
       ***
       <input
@@ -87,9 +115,9 @@ function SearchAndFilter() {
         name="startYear"
         type="text"
         placeholder="Type start year"
-        onChange={handleStartYearChange}
+        onChange={handleFormChange}
         onBlur={fixStartYear}
-        value={startYear}
+        value={formData.startYear}
       />
       -
       <input
@@ -97,9 +125,9 @@ function SearchAndFilter() {
         name="endYear"
         type="text"
         placeholder="Type end year"
-        onChange={handleEndYearChange}
+        onChange={handleFormChange}
         onBlur={fixEndYear}
-        value={endYear}
+        value={formData.endYear}
       />
       ***
       <input
@@ -107,9 +135,9 @@ function SearchAndFilter() {
         name="minMass"
         type="text"
         placeholder="Type min mass in grams"
-        onChange={handleMinMassChange}
+        onChange={handleFormChange}
         onBlur={fixMinMass}
-        value={minMass}
+        value={formData.minMass}
       />
       -
       <input
@@ -117,9 +145,9 @@ function SearchAndFilter() {
         name="maxMass"
         type="text"
         placeholder="Type max mass in grams"
-        onChange={handleMaxMassChange}
+        onChange={handleFormChange}
         onBlur={fixMaxMass}
-        value={maxMass}
+        value={formData.maxMass}
       />
     </form>
   );
