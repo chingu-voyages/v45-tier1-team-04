3dc83a28fb739fdd1eeb44cfd0f1c387fe407160
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-02 03:17:15 -0500

    Fix: <hr> cannot appear as a descendant of <p>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,11 +18,11 @@ function Card({ data }) {
         <center className="grid grid-cols-3 gap-y-12	">
           {data.map((dataObj) => {
             return (
-              <div key={dataObj.id}>
-                <p
-                  href="#"
-                  className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-                >
+              <div
+                key={dataObj.id}
+                className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+              >
+                <p href="#">
                   <span className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {dataObj.name}
                   </span>
@@ -47,11 +47,11 @@ function Card({ data }) {
                   <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.reclong}
                   </span>
-                  <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                    More
-                  </button>
                 </p>
+                <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                  More
+                </button>
               </div>
             );
           })}
