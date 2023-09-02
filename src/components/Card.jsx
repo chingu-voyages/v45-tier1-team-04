4b16de5731e1f958f01b674b02a48394b72b2dd5
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-02 03:14:45 -0500

    Fix: <div> cannot appear as a descendant of <p>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,30 +23,30 @@ function Card({ data }) {
                   href="#"
                   className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
                 >
-                  <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <span className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {dataObj.name}
-                  </div>
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                  </span>
+                  <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.nametype}
-                  </div>
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                  </span>
+                  <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.recclass}
-                  </div>
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                  </span>
+                  <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.mass}
-                  </div>
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                  </span>
+                  <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.fall}
-                  </div>
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                  </span>
+                  <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.year}
-                  </div>
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                  </span>
+                  <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.reclat}
-                  </div>
-                  <div className="font-normal text-gray-700 dark:text-gray-400">
+                  </span>
+                  <span className="block font-normal text-gray-700 dark:text-gray-400">
                     {dataObj.reclong}
-                  </div>
+                  </span>
                   <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                     More
