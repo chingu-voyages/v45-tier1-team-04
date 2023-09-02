cc4afa9f5d96fd3778210954b29ecc8747579991
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-02 03:37:05 -0500

    Refactor: SearchPage and Card components so each of them has one purpose

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,63 +1,37 @@
-import SearchAndFilter from "./SearchAndFilter";
-
-function Card({ data }) {
+function Card({ dataObj }) {
   return (
-    <>
-      <div className="App">
-        <h1 className="text-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          Fireball{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            {" "}
-            FETCH API
-          </span>
-        </h1>
-
-        {/*Search & Filter*/}
-        <SearchAndFilter />
-
-        <center className="grid grid-cols-3 gap-y-12	">
-          {data.map((dataObj) => {
-            return (
-              <div
-                key={dataObj.id}
-                className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-              >
-                <p href="#">
-                  <span className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {dataObj.name}
-                  </span>
-                  <span className="block font-normal text-gray-700 dark:text-gray-400">
-                    {dataObj.nametype}
-                  </span>
-                  <span className="block font-normal text-gray-700 dark:text-gray-400">
-                    {dataObj.recclass}
-                  </span>
-                  <span className="block font-normal text-gray-700 dark:text-gray-400">
-                    {dataObj.mass}
-                  </span>
-                  <span className="block font-normal text-gray-700 dark:text-gray-400">
-                    {dataObj.fall}
-                  </span>
-                  <span className="block font-normal text-gray-700 dark:text-gray-400">
-                    {dataObj.year}
-                  </span>
-                  <span className="block font-normal text-gray-700 dark:text-gray-400">
-                    {dataObj.reclat}
-                  </span>
-                  <span className="block font-normal text-gray-700 dark:text-gray-400">
-                    {dataObj.reclong}
-                  </span>
-                </p>
-                <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                  More
-                </button>
-              </div>
-            );
-          })}
-        </center>
-      </div>
-    </>
+    <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <p href="#">
+        <span className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {dataObj.name}
+        </span>
+        <span className="block font-normal text-gray-700 dark:text-gray-400">
+          {dataObj.nametype}
+        </span>
+        <span className="block font-normal text-gray-700 dark:text-gray-400">
+          {dataObj.recclass}
+        </span>
+        <span className="block font-normal text-gray-700 dark:text-gray-400">
+          {dataObj.mass}
+        </span>
+        <span className="block font-normal text-gray-700 dark:text-gray-400">
+          {dataObj.fall}
+        </span>
+        <span className="block font-normal text-gray-700 dark:text-gray-400">
+          {dataObj.year}
+        </span>
+        <span className="block font-normal text-gray-700 dark:text-gray-400">
+          {dataObj.reclat}
+        </span>
+        <span className="block font-normal text-gray-700 dark:text-gray-400">
+          {dataObj.reclong}
+        </span>
+      </p>
+      <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        More
+      </button>
+    </div>
   );
 }
 export default Card;
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,5 +1,6 @@
 import Card from "./Card";
 import { useState, useEffect } from "react";
+import SearchAndFilter from "./SearchAndFilter";
 
 function SearchPage() {
   const url = "https://data.nasa.gov/resource/gh4g-9sfh.json";
@@ -14,6 +15,27 @@ function SearchPage() {
     fetchInfo();
   }, []);
 
-  return <Card data={data} />;
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          Fireball{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            {" "}
+            FETCH API
+          </span>
+        </h1>
+        {/*Search & Filter*/}
+        <SearchAndFilter />
+
+        {/*Mapping all Cards*/}
+        <center className="grid grid-cols-3 gap-y-12	">
+          {data.map((dataObj) => {
+            return <Card key={dataObj.id} dataObj={dataObj} />;
+          })}
+        </center>
+      </div>
+    </>
+  );
 }
 export default SearchPage;
