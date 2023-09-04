d0f4846273808d3bb29be1a278e40c82b611a927
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-04 07:57:03 -0500

    Refactor: Card component to be more semantic and use a ul list

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,40 @@
 function Card({ dataObj }) {
   return (
     <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <p href="#">
-        <span className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {dataObj.name}
-        </span>
-        <span className="block font-normal text-gray-700 dark:text-gray-400">
-          {dataObj.nametype}
-        </span>
-        <span className="block font-normal text-gray-700 dark:text-gray-400">
-          {dataObj.recclass}
-        </span>
-        <span className="block font-normal text-gray-700 dark:text-gray-400">
-          {dataObj.mass}
-        </span>
-        <span className="block font-normal text-gray-700 dark:text-gray-400">
-          {dataObj.fall}
-        </span>
-        <span className="block font-normal text-gray-700 dark:text-gray-400">
-          {dataObj.year}
-        </span>
-        <span className="block font-normal text-gray-700 dark:text-gray-400">
-          {dataObj.reclat}
-        </span>
-        <span className="block font-normal text-gray-700 dark:text-gray-400">
-          {dataObj.reclong}
-        </span>
-      </p>
+      <ul>
+        <li>
+          <h2 className="text-2xl font-bold">{dataObj.name}</h2>
+        </li>
+        <li>
+          Name Type: <span className="font-bold">{dataObj.nametype}</span>
+        </li>
+        <li>
+          Class: <span className="font-bold">{dataObj.recclass}</span>
+        </li>
+        <li>
+          Mass:{" "}
+          <span className="font-bold">
+            {dataObj.mass > 1000
+              ? dataObj.mass / 1000 + "kg"
+              : dataObj.mass + "g"}
+          </span>
+        </li>
+        <li>
+          Fell/Found: <span className="font-bold">{dataObj.fall}</span>
+        </li>
+        <li>
+          Year:{" "}
+          <span className="font-bold">
+            {new Date(dataObj.year).getFullYear()}
+          </span>
+        </li>
+        <li>
+          Latitude: <span className="font-bold">{dataObj.reclat}</span>
+        </li>
+        <li>
+          Longitude: <span className="font-bold">{dataObj.reclong}</span>
+        </li>
+      </ul>
       <hr className="w-48 h-1 mx-auto my-2 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"></hr>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         More
