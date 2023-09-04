f177e33546f74aad95462b98a696763f96d1e19e
biroue <biroueisaac@gmail.com>
2023-09-04 21:57:07 +0100

    change the width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ dataObj }) {
   return (
-    <div className="min-w-[25%] block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div className="min-w-[20%] block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <ul>
         <li>
           <h2 className="text-2xl font-bold">{dataObj.name}</h2>
