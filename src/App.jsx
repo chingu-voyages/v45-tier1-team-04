632780570e0f11da1651cb92f82b84d71d099212
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-06 03:36:15 -0500

    Add: React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import BgVideo from "./components/BgVideo";
 import Header from "./components/Header";
-// import Landing from "./components/Landing";
 import Footer from "./components/Footer";
-// import Card from "./components/Card";
 import SearchPage from "./components/SearchPage";
 import Landing from "./components/Landing";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="w-[100vw] h-[100vh] overflow-hidden ">
       <BgVideo />
       <Header />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+
       <Footer />
     </div>
   );
