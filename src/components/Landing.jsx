571b84c8c3f3b22c162d8840e1104945658332f5
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-06 04:19:02 -0500

    Add: links to the Landing Page main buttons

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
+
 function Landing() {
   return (
-    <div className="relative z-10 w-full h-full flex flex-col justify-center transition-all duration-75">
+    <div className="relative z-10 w-full h-[80%] flex flex-col justify-center transition-all duration-75">
       <div className="overlay w-full  bg-[#00000050]"></div>
       <div className=" w-full text-white flex flex-col justify-center items-center absolute px-[10em]">
         <h1 className="text-[2.5rem] lg:text-[3rem] w-full text-center font-semibold">
@@ -13,10 +15,15 @@ function Landing() {
         </p>
         <div className="flex justify-between my-[2em] font-semibold gap-[2em] text-[1rem] lg:text-[1.3rem]">
           <div className="w-[350px] text-center px-[15px] py-[10px] border-2 border-black rounded-xl text-black font-semibold hover:bg-[#00000080] hover:text-white transition-all ease-in">
-            <a href="">Learn more</a>
+            <a
+              href="https://solarsystem.nasa.gov/asteroids-comets-and-meteors/overview/"
+              target="_blank"
+            >
+              Learn more
+            </a>
           </div>
           <div className="w-[350px] text-center px-[15px] py-[10px] border-2 border-black rounded-xl bg-[#00000080] text-white hover:bg-transparent hover:text-black transition-all ease-in">
-            <a href="/search">Search Meteorites</a>
+            <Link to="/search">Search Meteorites</Link>
           </div>
         </div>
       </div>
