5a7229ddefcab11bd3fc3db55c003e4beda638aa
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-06 03:56:17 -0500

    Add: Header Home and Search links using React Router <Link>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <nav className="flex justify-between w-full sticky z-20 top-0 font-extrabold xl:text-[1.2rem] lg:text-[1rem] sm:text-[.8rem] text-white border-b-2  px-14 py-6">
@@ -5,8 +7,12 @@ function Header() {
         Fireball
       </h1>
       <ul className="flex justify-center items-center gap-[1.5em] text-[.7rem] uppercase font-bold">
-        <li className="cursor-pointer">Home</li>
-        <li className="cursor-pointer">Search</li>
+        <li className="cursor-pointer">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="cursor-pointer">
+          <Link to="/search">Search</Link>
+        </li>
         <li className="cursor-pointer">Blogs</li>
       </ul>
     </nav>
