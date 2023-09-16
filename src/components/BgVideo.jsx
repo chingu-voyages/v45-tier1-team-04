ede57f761337fc0ed6a5198c0223929657fe38e6
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-16 05:51:11 -0500

    Fix: video background to have black background color

--- src/components/BgVideo.jsx
+++ src/components/BgVideo.jsx
@@ -2,7 +2,7 @@ import video from "../assets/bgVideo.mp4";
 
 function BgVideo() {
   return (
-    <div className="absolute w-full h-full z-0">
+    <div className="absolute w-full h-full z-0 bg-black">
       <video
         className="w-full h-full object-cover"
         src={video}
