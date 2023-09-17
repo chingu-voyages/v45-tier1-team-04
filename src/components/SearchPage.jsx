1d013568bb04c021f4d20294ca610d961536762c
Drazen Drinic <drazendrinic@hotmail.com>
2023-09-17 14:43:23 -0500

    Fix: last row of cards visibility

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -67,7 +67,7 @@ function SearchPage() {
 
   return (
     <>
-      <div className=" relative mt-4 z-10 flex flex-col justify-center w-full">
+      <div className=" relative mt-4 z-10 flex flex-col justify-center w-full pb-72">
         {/* <h1 className="text-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
           Fireball{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
